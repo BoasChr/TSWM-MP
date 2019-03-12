c0fc1e3ce734ca46d323971593e0ed542156993d
Emil <no>
2019-03-12 19:24:25 +0100

    Search engine virker + CSS update (design er lort)

--- mainpage.js
+++ mainpage.js
@@ -6,26 +6,38 @@
 //Check:
 //https://api.flickr.com/services/feeds/photos_public.gne?tags=dogss&format=json
 
-
 function jsonFlickrFeed(json) {
+        // Clear the 'gallery'
+        $('#gallery').html('');
 
-    //set the content of element with id "container" to empty
-    $('#right').html('');
+        if (json.items.length == 0) {
+            $("#results").text("0 results found.");
 
-    //for each item returned from json.items (from jsonFlickrFeed)
-    $.each(json.items, function (i, item) {
-        //findt the media item and append it to the element
-        //called "container" in your html
-        $("<img />").attr("src", item.media.m).appendTo("#right");
+            $("#searchList li:first").addClass("resultless");
+        }
+        else $("#results").text("");
 
-        //when there is 10 images stop
-        if (i == 9)
-            return false;
-    });
+        //for each item returned from json.items (from jsonFlickrFeed)
+        $.each(json.items, function (i, item) {
+            //findt the media item and append it to the element
+            //called "container" in your html
+            $("<img />").attr("src", item.media.m).appendTo("#gallery");
+
+            //when there is 10 images stop
+            if (i == 10) return false;
+            
+        });
 };
 
-//When pressing on element with class "city"
-$(".test").on('click', function (e) {
+// When pressing on list element we will search for it using its class
+// Using document so we are able to find dynamically added items --
+// -- define what we are looking for after event
+$(document).on('click', "li", function (e) {
+
+    if ($(this).hasClass("resultless")){
+        $("#results").text("Unable to find results for '" + $(this).attr('id') + "'.");
+        return false;
+    }
 
     //The $.ajax() method with the form $.ajax(url [,settings]) (slide 29)
     //This method returns the URL to jsonFlickrFeed in the form of
@@ -35,7 +47,9 @@ $(".test").on('click', function (e) {
         {
             dataType: 'jsonp',
             //The tag to search for is the id of the currently pressed city.
-            data: { "tags": this.id, "format": "json" }
+            data: { "tags": e.target.id, "format": "json" },
         }
     );
+    $("li").removeClass("current");
+    $(this).addClass("current");
 });
