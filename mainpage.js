42f1c7fc721f047cf3780e9953392921f6ea6b72
Emil <no>
2019-03-19 16:01:24 +0100

    Geolocation + Ajax + comments

--- mainpage.js
+++ mainpage.js
@@ -1,62 +1,62 @@
-$(function() {
-    var lat, long, cityName = "roses"; // Default search value if we don't find a city
+$(function() { // When site has loaded (document ready)
+    var lat, long; // declare variables
 
-    if (Modernizr.geolocation) {
-        navigator.geolocation.getCurrentPosition(success, fail);
+    if (Modernizr.geolocation) { // If browser supports geolocation
+        navigator.geolocation.getCurrentPosition(success, fail); // Attempt to get geolocation
     }
     
-    function success(position) {
+    function success(position) { // If we were succesfull
+        // save coordinates into respective variable
         long = position.coords.longitude;
         lat = position.coords.latitude;
 
+        // Custom url to ping, using coordinates
         var geoUrl = "https://geocode.xyz/" + lat + "," + long + "?json=1";
 
+        // Ajax request of Json data
         $.ajax({
             type: "GET",
             dataType: 'jsonp',
-            url: geoUrl,
-            async: false,
+            url: geoUrl, 
+            async: true,
             contentType: "application/json; charset=utf-8",
-            success: function (data) {
-                cityName = data.city;
+            
+           
+            success: function (data) { // If successful, store response into 'data'
     
-                $.ajax('https://api.flickr.com/services/feeds/photos_public.gne', {
+                $.ajax('https://api.flickr.com/services/feeds/photos_public.gne', { // start a Flickr Ajax request ...
                     dataType: 'jsonp',
-                    //The tag to search for is the id of the currently pressed city.
-                    data: { "tags": cityName, "format": "json" },
+                    // ... using the 'city' attribute from the received data
+                    data: { "tags": data.city, "format": "json" },
                 });
             }
-
         });
     }
 
-    function fail(error) {
-        console.warn(error.code + ': ' + error.message);
+    function fail() { // Failed to get Geolocation - not really of importance to us
     }
-
 });
 
 
-//jsonFlickrFeed is a callback function that we get from the
-//documentation from flickr.com
-//it contains all the data that we want to access
+//jsonFlickrFeed is a callback function that we get from the flickr.com documentation 
+// it contains all the data that we want to access
 function jsonFlickrFeed(json) {
         // Clear the 'gallery'
         $('#gallery').html('');
 
-        if (json.items.length == 0) {
-            $("#results").text("0 results found.");
+        if (json.items.length == 0) { // If we didn't receive any results from the request
+            $("#results").text("0 results found."); // Feedback for the user
 
-            $("#searchList li:first").addClass("resultless");
+            $("#searchList li:first").addClass("resultless"); // Add the search anyway but with class 'resultless'
         }
-        else $("#results").text("");
+        else $("#results").text(""); // Received some results, just show pictures not text.
 
-        //for each item returned from json.items (from jsonFlickrFeed)
+        // For each item returned from json.items
         $.each(json.items, function (i, item) {
-            //find the media item and append it to the element called "gallery" in the html
+            // find the media item (contains picture url) and append it to the element called "gallery"
             $("<img />").attr("src", item.media.m).appendTo("#gallery");
 
-            //when there are 20 images stop
+            // when there are 20 images stop
             if (i == 20) return false;
             
         });
@@ -91,11 +91,11 @@ $(document).on('click', "li", function (e) {
 //Function called by the clear history button in mainpage.html
 function clearHistory(){
         if(searches < 1){
-        alert("There are no searches to delete");
+        alert("There are no searches to delete.");
         return;
     }
     
-    //Remove all items from the li object, empty the gallery, set the label
+    // Remove all items of type li object, empty the gallery, set the label
     $("li").remove();
     searchLabel = "History cleared";
     searches = 0;
