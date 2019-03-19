77042934fcbfa919315ce91062454f170e92a5c8
Emil <no>
2019-03-19 11:36:43 +0100

    Hvad fanden foregår der med divs

--- animation.js
+++ animation.js
@@ -1,12 +1,15 @@
 
 function animateWelcome() {
 
-    $('#animation').fadeIn(2000);
+    $('#animation').fadeIn(0);
 
     $('#animation').animate({ 
-        top: "5%",
+        position: "absolute",
+        paddingTop: "0",
         fontSize: "100%",
         position: "static",
+        top: "2%",
+        
         }, 850, 'swing', function () { //declare function when animation is done
         fadeRestIn();
     });
@@ -16,15 +19,15 @@ function animateWelcome() {
 function fadeRestIn(){
    
     $('#clearButton').fadeIn(1500);
-    $('#mainsection').fadeIn(1500);
+    $('#mainSection').fadeIn(1500);
     $('#searchBar').fadeIn(1500);
 
 }
 
 
 $(document).ready(function () {
- // $('#flick').hide();
-    $('#mainsection').hide();
+//  $('#flick').hide();
+    $('#mainSection').hide();
     $('#searchBar').hide();
     $('#clearButton').hide();
     $('#animation').hide();
